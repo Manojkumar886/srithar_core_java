1f498bcb89f540e5ac8c3cbf46f4ec6a45dd5fb4
Manojkumar886 <maddymano434@gmail.com>
2024-01-20 13:15:51 +0530

    javascript basics is over`

---
+++ src/main/java/fundamentaljava/HTML/JAVASCRIPT/SampleFunction.js
@@ -0,0 +1,13 @@
+function addition(a,b)
+{
+    let connection = sa + b; 
+
+    alert(" my a value is :"+a+" \n my b value is  :"+b+" \n a+b = "+connection)
+    
+}
+
+
+let content = (mycontent) =>
+{
+     return mycontent;
+}
